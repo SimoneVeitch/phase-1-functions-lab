902ac8fe13ccb2c0a412dd681b40cd166a92f9a8
Simone Veitch <simone.veitch@gmail.com>
2024-02-15 11:38:37 +1100

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks (pickupLocation) {
+    const hqLocation = 42;
+    return Math.abs(pickupLocation - hqLocation);
+}
+
+function distanceFromHqInFeet (pickupLocation) {
+    const blocks = distanceFromHqInBlocks (pickupLocation);
+    const feetPerBlock = 264;
+    return blocks * feetPerBlock;
+}
+
+function distanceTravelledInFeet (start, destination) {
+    const feetPerBlock = 264;
+    return (Math.abs(start - destination)) * feetPerBlock;
+}
+
+function calculatesFarePrice(start,destination) {
+    const feetPerBlock = 264;
+    const distance = (Math.abs(start - destination)) * feetPerBlock;
+    let fare = 0;
+
+    if (distance <= 400) {
+        fare = 0;
+    } else if (distance > 400 && distance <= 2000) {
+        fare = (distance - 400) * 0.02;
+    } else if (distance > 2000 && distance <2500) {
+        fare = 25;
+    } else {
+        return 'cannot travel that far';
+    }
+    return fare;
+   }
+   
